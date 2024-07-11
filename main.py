7cf2d9e2dd719bf7ab2f28fb83e4b7656b3a6f0d
Dagan Vega <daganjv.pro@gmail.com>
2024-07-11 12:44:34 -0600

    Añadido de pestaña de inicio

--- main.py
+++ main.py
@@ -2,8 +2,14 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 @app.route("/")
+
 def hello():
     return render_template("datosu.html")
+
 @app.route("/CambiarContraseña")
 def CambiarContraseña():
     return render_template("CambiarContraseña.html")
+
+@app.route("/inicio")
+def inicio():
+    return render_template("inicio.html")
