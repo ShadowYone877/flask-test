d84f30c0869d455b336987234fd8434855886414
Jorge <yonemotoh.jorgeg@gmail.com>
2024-07-11 10:52:52 -0600

    proyectoincial

---
+++ main.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+@app.route("/")
+def hello():
+    return render_template("datosu.html")
+@app.route("/CambiarContraseña")
+def CambiarContraseña():
+    return render_template("CambiarContraseña.html")
