923de52a24dd0fe7718e7cbd2edc051fe860266c
Isaac F <josuei.fm@iztapalapa.tecnm.mx>
2024-07-18 15:35:32 -0600

    /

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("inicio.html")
+
 @app.route("/datosusuario")
 def hello():
     return render_template("datosu.html")
