a764fdebef35d4ff60be936ad1bf1e35012dec09
Regina Diaz <regval3307@gmail.com>
2024-08-06 01:54:08 -0600

    validacion del campo de cp

--- app/addressForm.py
+++ app/addressForm.py
@@ -10,5 +10,5 @@ class addressForm(FlaskForm):
     state = StringField('Estado:', validators=[DataRequired(message='Campo requerido'), Length(max=80)])
     municipality = StringField('Municipio / Alcaldia', validators=[DataRequired(message='Campo requerido')])
     cologne = StringField('Colonia', validators=[DataRequired(message='Campo requerido')])
-    cp = StringField('Codigo Postal', validators=[DataRequired(),Length(min=5, max=5, message='Requiere este campo minimo 5 numeros')])
+    cp = StringField('Codigo Postal', validators=[DataRequired(message='Campo requerido'),Length(min=5, max=5, message='Requiere este campo minimo 5 numeros')])
     submit = SubmitField('Siguiente')
